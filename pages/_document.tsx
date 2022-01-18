7d11a6b114bc4a4ce949c562e05f541058dd6b3d
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-01-18 05:20:15 -0300

    custom nextjs document to deal with emotion styling cache that is required by mui component library

---
+++ pages/_document.tsx
@@ -0,0 +1,89 @@
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+import createEmotionServer from "@emotion/server/create-instance";
+import createCachce from "@emotion/cache";
+import React from "react";
+
+class CustomDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang="pt-br">
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/favicon/site.webmanifest" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+CustomDocument.getInitialProps = async (ctx) => {
+  const originalRenderPage = ctx.renderPage;
+
+  const cache = createCachce({ key: "css" });
+  const { extractCriticalToChunks } = createEmotionServer(cache);
+
+  ctx.renderPage = () => {
+    return originalRenderPage({
+      // eslint-disable-next-line react/display-name
+      enhanceApp: (App: any) => (props: any) =>
+        <App emotionCache={cache} {...props} />,
+    });
+  };
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  const emotionStyles = extractCriticalToChunks(initialProps.html);
+
+  const emotionStyleTags = emotionStyles.styles.map((style) => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(" ")}`}
+      key={style.key}
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ));
+
+  return {
+    ...initialProps,
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      ...emotionStyleTags,
+    ],
+  };
+};
+
+export default CustomDocument;
