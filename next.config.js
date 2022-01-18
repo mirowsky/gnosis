06ca8c3b5931cf6c42244ebf854fcb39266b7f84
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-01-18 04:31:11 -0300

    .env and GTM env utility configuration at nextjs.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
+require("dotenv").config({ path: `./.env` });
+
+function getEnvironmentGTM() {
+  return `${process.env[`GTM_${process.env.GTM_ENV.toUpperCase()}`]}`;
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    getEnvironmentGTM()
+  },
+  swcMinify: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
