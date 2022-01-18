e08baafb6754b1c4c9971e79b10cec260090d8ad
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-01-18 02:58:02 -0300

    stylesheet helper, cleanup default nextjs files and set default breakpoints in a separated file

---
+++ theme/breakpoints.ts
@@ -0,0 +1,14 @@
+const breakpointKeys = ['xs', 'sm', 'md', 'lg', 'xl'] as const;
+
+export type BreakpointKeys = typeof breakpointKeys[number];
+
+export type CoreBreakpoints = { [Key in BreakpointKeys]: number };
+
+const breakpoints: CoreBreakpoints = {
+  xs: 320,
+  sm: 640,
+  md: 1024,
+  lg: 1170,
+  xl: 1400,
+};
+export default breakpoints;
---
+++ theme/stylesheet.ts
@@ -0,0 +1,4 @@
+import {createStyleSheet} from '@mirowsky/mui-stylesheet'
+import theme from './theme'
+
+export const stylesheet = createStyleSheet(theme);
