9d0eb9ab02d91a1b3e56390f8ba924e78d612746
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-01-19 17:05:01 -0300

    included alias resolver at storybook main config file to allow it to resolve ts path

--- .storybook/main.js
+++ .storybook/main.js
@@ -16,6 +16,7 @@ module.exports = {
           ...config.resolve.alias,
           // tsconfig.json path alias wont be resolved without this, webpack will try to find it inside the node_modules folder as it is an absolute import
           "theme-stylesheet": path.join(process.cwd(), "theme/stylesheet.ts"),
+          "@workspace/utility": path.join(process.cwd(), "utility/index.ts"),
         },
       },
     };
