285a258ac6965b09b7d376617483281d6657f348
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-01-20 13:27:03 -0300

    mobile drawer stories

---
+++ components/Layout/MobileDrawer/MobileDrawer.stories.tsx
@@ -0,0 +1,24 @@
+import { Story, Meta } from "@storybook/react";
+import { MobileMenu, MobileMenuProps } from "./MobileDrawer";
+
+export default {
+  component: MobileMenu,
+  title: "MobileDrawer",
+} as Meta;
+
+const Template: Story<MobileMenuProps> = (args) => <MobileMenu {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  menuItems: [
+    { label: "Serviços", onClick: () => console.log("Clicked") },
+    { label: "Sobre nós", onClick: () => console.log("Clicked") },
+    { label: "Blog", onClick: () => console.log("Clicked") },
+    { label: "Contato", onClick: () => console.log("Clicked") },
+  ],
+  logo: {
+    src: "./images/logo.svg",
+    alt: "Hassan Fisioterapia - Logotipo",
+  },
+  open: true,
+};
