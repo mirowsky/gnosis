65de76b97cf33e9c78f00d580b26b08998df4ceb
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-01-20 18:01:04 -0300

    test cases for footer component

--- components/Layout/Footer/Footer.test.tsx
+++ components/Layout/Footer/Footer.test.tsx
@@ -1,10 +1,76 @@
 import { render } from "@testing-library/react";
 import Footer, { FooterProps } from "./Footer";
+import { DEFAULT_FOOTER_TEST_PROPS, FOOTER_TEST_ID } from "./constants";
 
 describe("Footer core component", () => {
   it("should render the component", () => {
-    const { baseElement } = render(<Footer />);
+    const { baseElement } = render(<Footer {...DEFAULT_FOOTER_TEST_PROPS} />);
 
     expect(baseElement).toBeInTheDocument();
+  });
+});
+
+describe("Footer contact list", () => {
+  const { getByTestId, baseElement } = render(
+    <Footer {...DEFAULT_FOOTER_TEST_PROPS} />
+  );
+
+  it("should render", () => {
+    expect(baseElement).toBeInTheDocument();
+  });
+
+  it("should render all contacts", () => {
+    const childNodes = getByTestId(
+      FOOTER_TEST_ID.CONTACTS_CONTAINER
+    ).childNodes;
+
+    childNodes.forEach((childNode, index) => {
+      expect(childNode).toHaveTextContent(
+        DEFAULT_FOOTER_TEST_PROPS.contacts[index]
+      );
+    });
+  });
+});
+
+describe("Footer email list", () => {
+  const { getByTestId, baseElement } = render(
+    <Footer {...DEFAULT_FOOTER_TEST_PROPS} />
+  );
+
+  it("should render", () => {
+    expect(baseElement).toBeInTheDocument();
+  });
+
+  it("should render all emails acquired via props", () => {
+    const childNodes = getByTestId(
+      FOOTER_TEST_ID.EMAIL_CONTACT_CONTAINER
+    ).childNodes;
+
+    childNodes.forEach((childNode, index) => {
+      expect(childNode).toHaveTextContent(
+        DEFAULT_FOOTER_TEST_PROPS.emailContacts[index]
+      );
+    });
+  });
+});
+
+describe("Footer social icons", () => {
+  const { getByTestId, baseElement } = render(
+    <Footer {...DEFAULT_FOOTER_TEST_PROPS} />
+  );
+
+  it("should have a container that will host the list", () => {
+    expect(baseElement).toBeInTheDocument();
+  });
+
+  it("should render the social icons within the footer", () => {
+    const childNodes = getByTestId(FOOTER_TEST_ID.SOCIALS_CONTAINER).childNodes;
+
+    childNodes.forEach((childNode, index) => {
+      expect(childNode).toHaveAttribute(
+        "src",
+        DEFAULT_FOOTER_TEST_PROPS.socials[index]
+      );
+    });
   });
 });
--- components/Layout/Footer/Footer.tsx
+++ components/Layout/Footer/Footer.tsx
@@ -1,9 +1,23 @@
+import { Box } from "@mui/material";
+import stylesheet from "@workspace/stylesheet";
 import React from "react";
 
-export interface FooterProps {}
+export interface FooterProps {
+  copyright: string;
+  contacts: string[];
+  emailContacts: string[];
+  socials: Array<{
+    icon: React.FunctionComponent;
+    onClick: (...args: unknown[]) => void;
+  }>;
+}
 
 const Footer = (props: FooterProps) => {
-  return <div></div>;
+  return <Box sx={styles.root}></Box>;
 };
 
 export default Footer;
+
+const styles = stylesheet.create({
+  root: {},
+});
