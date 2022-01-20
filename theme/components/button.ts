b61cbe4a348c1bfabdf3d938c4e23fb40a939902
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-01-20 12:47:50 -0300

    default button styles override

--- theme/components/button.ts
+++ theme/components/button.ts
@@ -14,7 +14,7 @@ export const buttonOverride = (theme: Theme): Components["MuiButton"] => {
       },
 
       sizeMedium: {
-        fontSize: theme.spacing(1 * 2.75),
+        fontSize: theme.spacing(1 * 2.5),
       },
 
       sizeLarge: {
@@ -27,27 +27,8 @@ export const buttonOverride = (theme: Theme): Components["MuiButton"] => {
       },
 
       text: {
-        padding: "0.45em 2.25em",
+        padding: `${theme.spacing(1 * 0.45)} ${theme.spacing(1 * 4)}`,
         fontWeight: 600,
-        fontSize: "1em",
-      },
-
-      textPrimary: {
-        padding: "0.45em 2.25em",
-        fontWeight: 600,
-        fontSize: "1em",
-        color: theme.palette.primary.dark,
-      },
-
-      outlinedPrimary: {
-        border: `3px solid ${theme.palette.primary.main}`,
-        fontWeight: 500,
-        padding: "0.45em 3em",
-        backgroundColor: "transparent",
-
-        ":hover": {
-          border: `3px solid ${theme.palette.primary.dark}`,
-        },
       },
     },
   };
