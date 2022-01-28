01640ed28cf4d5aea011d5ce9996c3ee5e4fe4fc
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-01-28 19:39:43 -0300

    example demonstration on how to use the theme aware fluid typography

--- theme/typography.ts
+++ theme/typography.ts
@@ -6,6 +6,7 @@ import {
 import breakpoints from "./breakpoints";
 import { Properties } from "csstype";
 import { Head } from "@workspace/types";
+import { themeFluidTypography } from "./createFluidTypography";
 
 const responsiveFontSize = themeResponsiveStyles("fontSize");
 const resposiveLineHeight = themeResponsiveStyles("lineHeight");
@@ -28,9 +29,7 @@ const fontStyles = titleStyles({
 
 const typography: ThemeOptions["typography"] = {
   fontFamily: ["Inter", "sans-serif"].join(","),
-  htmlFontSize: 10,
-  fontSize: 10,
-  h1: fontStyles({ xs: "42px", lg: "64px" }),
+  h1: { fontSize: themeFluidTypography({ maxFontSize: 64, minFontSize: 32 }) },
   h5: fontStyles({ xs: "18px", lg: "22px" }),
   caption: fontStyles({ xs: "12px", lg: "14px" }),
 };
