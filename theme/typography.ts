cfa7c31eaf600bf4fb3a6c2b1bea1f9d6f10404b
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-01-28 19:41:28 -0300

    clean up at typography file

--- theme/typography.ts
+++ theme/typography.ts
@@ -1,36 +1,12 @@
 import { ThemeOptions } from "@mui/material";
-import {
-  createResponsiveStyle,
-  themeResponsiveStyles,
-} from "./createResponsiveProperty";
-import breakpoints from "./breakpoints";
-import { Properties } from "csstype";
-import { Head } from "@workspace/types";
 import { themeFluidTypography } from "./createFluidTypography";
-
-const responsiveFontSize = themeResponsiveStyles("fontSize");
-const resposiveLineHeight = themeResponsiveStyles("lineHeight");
-
-const titleStyles = (styles: Properties) => {
-  return (args: Head<typeof responsiveFontSize>) => {
-    const fontSizes = responsiveFontSize(args);
-
-    return { ...fontSizes, ...styles };
-  };
-};
-
-const displayStyle = titleStyles({
-  fontWeight: "bold",
-});
-
-const fontStyles = titleStyles({
-  textDecoration: "none",
-});
 
 const typography: ThemeOptions["typography"] = {
   fontFamily: ["Inter", "sans-serif"].join(","),
   h1: { fontSize: themeFluidTypography({ maxFontSize: 64, minFontSize: 32 }) },
-  h5: fontStyles({ xs: "18px", lg: "22px" }),
-  caption: fontStyles({ xs: "12px", lg: "14px" }),
+  h5: { fontSize: themeFluidTypography({ maxFontSize: 22, minFontSize: 18 }) },
+  caption: {
+    fontSize: themeFluidTypography({ maxFontSize: 14, minFontSize: 12 }),
+  },
 };
 export default typography;
