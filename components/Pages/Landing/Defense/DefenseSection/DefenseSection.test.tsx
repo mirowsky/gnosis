6b7bc18bfee2dda3c84c750b2952556d9fef6e6e
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-01-30 04:30:14 -0300

    test cases for defense section all passing

--- components/Pages/Landing/Defense/DefenseSection/DefenseSection.test.tsx
+++ components/Pages/Landing/Defense/DefenseSection/DefenseSection.test.tsx
@@ -1,5 +1,9 @@
 import { render } from "@testing-library/react";
-import { DEFAULT_DEFENSE_SECTION_TEST_PROPS } from "./constants";
+import { DEFENSE_CARD_TEST_ID } from "../DefenseCard/constants";
+import {
+  DEFAULT_DEFENSE_SECTION_TEST_PROPS,
+  DEFENSE_SECTION_TEST_ID,
+} from "./constants";
 import DefenseSection, { DefenseSectionProps } from "./DefenseSection";
 
 const setup = (
@@ -15,5 +19,19 @@ describe("Defense section", () => {
     const { baseElement } = setup();
 
     expect(baseElement).toBeInTheDocument();
+  });
+
+  it("should have display all the defense cards received via props", () => {
+    const { getByTestId } = setup();
+
+    const containerElement = getByTestId(
+      DEFENSE_SECTION_TEST_ID.GRID_CONTAINER
+    );
+
+    const cards = containerElement.childNodes;
+
+    cards.forEach((node, index) => {
+      expect(node).toBeInTheDocument();
+    });
   });
 });
--- components/Pages/Landing/Defense/DefenseSection/DefenseSection.tsx
+++ components/Pages/Landing/Defense/DefenseSection/DefenseSection.tsx
@@ -1,15 +1,47 @@
-import { Box } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import stylesheet from "@workspace/stylesheet";
 import React from "react";
+import DefenseCard, { DefenseCardProps } from "../DefenseCard/DefenseCard";
+import { DEFENSE_SECTION_TEST_ID } from "./constants";
 
-export type DefenseSectionProps = {};
+export type DefenseSectionProps = {
+  items: DefenseCardProps[];
+};
 
-const DefenseSection = (props: DefenseSectionProps) => {
-  return <Box sx={styles.root}></Box>;
+const DefenseSection = ({ items = [] }: DefenseSectionProps) => {
+  return (
+    <Container maxWidth="lg" sx={styles.root}>
+      <Box
+        data-testid={DEFENSE_SECTION_TEST_ID.GRID_CONTAINER}
+        sx={styles.grid}
+      >
+        {items.map((value, index) => {
+          return <DefenseCard key={index} {...value} />;
+        })}
+      </Box>
+    </Container>
+  );
 };
 
 export default DefenseSection;
 
 const styles = stylesheet.create({
-  root: {},
+  root: {
+    height: "auto",
+    width: "100%",
+  },
+  grid: {
+    display: "grid",
+    gridTemplateColumns: {
+      xs: "minmax(0, 1fr)",
+      sm: "minmax(0, 0.5fr) minmax(0, 0.5fr)",
+      lg: "minmax(0, 0.333fr) minmax(0, 0.333fr) minmax(0, 0.333fr)",
+    },
+    gridAutoFlow: "row",
+    width: "100%",
+    height: "100%",
+    gap: (theme) => ({ xs: theme.spacing(6) }),
+  },
+
+  item: {},
 });
