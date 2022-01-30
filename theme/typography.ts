b0bd227fa173886b9173985d836020d258814fee
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-01-29 21:53:51 -0300

    additional typography configuration

--- theme/typography.ts
+++ theme/typography.ts
@@ -1,10 +1,19 @@
 import { ThemeOptions } from "@mui/material";
 import { themeFluidTypography } from "./createFluidTypography";
-import { themeResponsiveStyles } from "./createResponsiveProperty";
 
 const typography: ThemeOptions["typography"] = {
   fontFamily: ["Inter", "sans-serif"].join(","),
   h1: { fontSize: themeFluidTypography({ maxFontSize: 64, minFontSize: 32 }) },
   h2: { fontSize: themeFluidTypography({ maxFontSize: 36, minFontSize: 24 }) },
+  h3: { fontSize: themeFluidTypography({ maxFontSize: 28, minFontSize: 22 }) },
+  h4: { fontSize: themeFluidTypography({ maxFontSize: 22, minFontSize: 20 }) },
+  h5: { fontSize: themeFluidTypography({ maxFontSize: 20, minFontSize: 18 }) },
+  h6: { fontSize: themeFluidTypography({ maxFontSize: 18, minFontSize: 16 }) },
+  body1: { fontSize: "1rem" },
+  body2: { fontSize: "0.875rem" },
+  subtitle1: { fontSize: "1.125rem" },
+  subtitle2: { fontSize: "0.875rem" },
+  overline: { fontSize: "0.75rem" },
+  caption: { fontSize: "0.75rem" },
 };
 export default typography;
