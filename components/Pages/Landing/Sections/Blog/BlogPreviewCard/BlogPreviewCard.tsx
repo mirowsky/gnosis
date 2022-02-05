af300fded11ac18c52bf9de35b3d12b2b2d7b2a5
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-02-05 05:14:37 -0300

    [FEATURE] Initial implementation with responsive styles for blog preview card

--- components/Pages/Landing/Sections/Blog/BlogPreviewCard/BlogPreviewCard.tsx
+++ components/Pages/Landing/Sections/Blog/BlogPreviewCard/BlogPreviewCard.tsx
@@ -1,15 +1,101 @@
-import { Box } from "@mui/material";
+import { WatchLater } from "@mui/icons-material";
+import { Box, Typography, Chip } from "@mui/material";
+import { ThemeProvider } from "@mui/system";
 import stylesheet from "@workspace/stylesheet";
 import React from "react";
 
-export type BlogPreviewCardProps = {};
+export type BlogPreviewCardProps = {
+  readingTime: string;
+  img: {
+    src: string;
+    alt: string;
+  };
+  title: string;
+  tags: string[];
+};
 
-const BlogPreviewCard = (props: BlogPreviewCardProps) => {
-  return <Box sx={styles.root}></Box>;
+const BlogPreviewCard = ({
+  img = {
+    src: "https://via.placeholder.com/500",
+    alt: "This is a placeholder, replace me",
+  },
+  readingTime = "10 min de leitura",
+  tags = [],
+  title = "Placeholder title",
+}: BlogPreviewCardProps) => {
+  return (
+    <Box sx={styles.root}>
+      <Box sx={styles.timeContainer}>
+        <Box component={WatchLater} sx={styles.timeIcon} />
+        <Typography variant="subtitle2" sx={styles.timeLabel}>
+          {readingTime}
+        </Typography>
+      </Box>
+
+      <Box sx={styles.pictureContainer}>
+        <Box component="img" sx={styles.picture} src={img.src} alt={img.alt} />
+      </Box>
+
+      <Box sx={styles.titleContainer}>
+        <Typography variant="h5" sx={styles.title}>
+          {title}
+        </Typography>
+      </Box>
+
+      <Box sx={styles.tagsContainer}>
+        {tags.map((tag, index) => {
+          return <Chip clickable label={tag} key={index} />;
+        })}
+      </Box>
+    </Box>
+  );
 };
 
 export default BlogPreviewCard;
 
 const styles = stylesheet.create({
-  root: {},
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    fontSize: (theme) =>
+      theme.fluidTypography({ maxFontSize: 16, minFontSize: 16 * 0.75 }),
+    gap: (theme) => theme.spacing(2),
+  },
+
+  // Container
+  timeContainer: {
+    display: "flex",
+    alignItems: "center",
+    gap: (theme) => theme.spacing(1),
+  },
+  pictureContainer: {
+    width: "26.25em",
+    height: "18.45em",
+  },
+  titleContainer: {},
+  tagsContainer: {},
+
+  // Item
+  tag: {
+    bgcolor: (theme) => theme.palette.primary.main,
+    color: (theme) => theme.palette.primary.contrastText,
+    fontWeight: 700,
+  },
+  picture: {
+    objectFit: "cover",
+    objectPosition: "center",
+    width: "100%",
+    height: "100%",
+    borderRadius: (theme) => theme.shape.borderRadius,
+  },
+  title: {
+    color: (theme) => theme.palette.primary.main,
+    fontWeight: 700,
+  },
+  timeIcon: {
+    color: (theme) => theme.palette.secondary.main,
+  },
+  timeLabel: {
+    color: (theme) => theme.palette.grey[700],
+  },
 });
--- components/Pages/Landing/Sections/Blog/BlogPreviewCard/constants.ts
+++ components/Pages/Landing/Sections/Blog/BlogPreviewCard/constants.ts
@@ -1,5 +1,13 @@
 import { BlogPreviewCardProps } from "./BlogPreviewCard";
 
-export const DEFAULT_BLOG_PREVIEW_CARDS_TEST_PROPS: BlogPreviewCardProps = {};
+export const DEFAULT_BLOG_PREVIEW_CARDS_TEST_PROPS: BlogPreviewCardProps = {
+  img: {
+    src: "https://via.placeholder.com/500",
+    alt: "This is a placeholder, replace me",
+  },
+  readingTime: "10 min de leitura",
+  tags: [],
+  title: "Placeholder title",
+};
 
 export const BLOG_PREVIEW_CARD_TEST_ID = {} as const;
