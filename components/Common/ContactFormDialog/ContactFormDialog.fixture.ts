31321c7bb001e7b41609ab1fc7423f2bc1ec8b6d
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-02-09 05:15:24 -0300

    [FEATURE] Contact Form dialog implementation

--- components/Common/ContactFormDialog/ContactFormDialog.fixture.ts
+++ components/Common/ContactFormDialog/ContactFormDialog.fixture.ts
@@ -1,6 +1,30 @@
 import type { ContactFormDialogProps } from "./ContactFormDialog";
 
-export const DEFAULT_ContactFormDialog_TESTING_PROPS: ContactFormDialogProps =
-  {};
+export const DEFAULT_ContactFormDialog_TESTING_PROPS: ContactFormDialogProps = {
+  title: "Placeholder title",
+  subtitle:
+    "Lorem ipsum dolum salet, please fill out this fields this is placeholder text to give context about this forms purpose to the end user.",
+  cancelLabel: "Cancel",
+  submitLabel: "Send",
+  onSubmit: () => {},
+  handleClose: () => {},
+  open: true,
+  emailInputProps: {
+    label: "Email",
+    placeholder: "Ex. joao.vitor@gmail.com",
+  },
+  messageInputProps: {
+    label: "Mensagem",
+    placeholder: "Ex: Tenho dúvidas a respeito de...",
+  },
+  nameInputProps: {
+    label: "Nome completo",
+    placeholder: "Ex. João Vitor",
+  },
+  phoneInputProps: {
+    label: "Número",
+    placeholder: "Ex: (00) 0-0000-0000",
+  },
+};
 
 export const ContactFormDialog_TEST_ID = {} as const;
--- components/Common/ContactFormDialog/ContactFormDialog.tsx
+++ components/Common/ContactFormDialog/ContactFormDialog.tsx
@@ -1,15 +1,123 @@
-import { Box } from "@mui/material";
-import stylesheet from "@workspace/stylesheet";
 import React from "react";
+import {
+  Button,
+  TextField,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Box,
+  DialogProps,
+  TextFieldProps,
+} from "@mui/material";
 
-export type ContactFormDialogProps = {};
+/* eslint-disable-next-line */
+export interface ContactFormDialogProps {
+  title?: string;
+  subtitle?: string;
+  cancelLabel?: string;
+  submitLabel?: string;
+  onSubmit?: (...args: unknown[]) => void;
+  handleClose?: (...args: unknown[]) => void;
+  open?: boolean;
+  nameInputProps?: TextFieldProps;
+  phoneInputProps?: TextFieldProps;
+  emailInputProps?: TextFieldProps;
+  messageInputProps?: TextFieldProps;
+  DialogProps?: DialogProps;
+}
 
-export const ContactFormDialog = (props: ContactFormDialogProps) => {
-  return <Box sx={styles.root}></Box>;
-};
+export function ContactFormDialog({
+  title = "Placeholder title",
+  subtitle = "Lorem ipsum dolum salet, please fill out this fields this is placeholder text to give context about this forms purpose to the end user.",
+  cancelLabel = "Cancel",
+  submitLabel = "Send",
+  onSubmit,
+  handleClose,
+  open = false,
+  emailInputProps = {
+    label: "Email",
+    placeholder: "Ex. joao.vitor@gmail.com",
+  },
+  messageInputProps = {
+    label: "Mensagem",
+    placeholder: "Ex: Tenho dúvidas a respeito de...",
+  },
+  nameInputProps = {
+    label: "Nome completo",
+    placeholder: "Ex. João Vitor",
+  },
+  phoneInputProps = {
+    label: "Número",
+    placeholder: "Ex: (00) 0-0000-0000",
+  },
+  DialogProps,
+}: ContactFormDialogProps) {
+  return (
+    <Dialog
+      sx={{
+        ".MuiDialog-paper": {
+          minWidth: "50%",
+        },
+      }}
+      maxWidth={"xl"}
+      open={open}
+      onClose={handleClose}
+      {...DialogProps}
+    >
+      <DialogTitle>{title}</DialogTitle>
+      <DialogContent>
+        <DialogContentText sx={{ pb: "2rem" }}>{subtitle}</DialogContentText>
+        <Box
+          sx={{
+            display: "grid",
+            gridTemplateColumns: { xs: "1fr", md: "1fr 1fr" },
+            gridTemplateRows: "1fr",
+            gridAutoFlow: "row",
+            gap: "3rem",
+          }}
+        >
+          <TextField
+            {...nameInputProps}
+            sx={{ gridColumn: { xs: "1/3", md: "initial" } }}
+            autoFocus
+            margin="dense"
+            fullWidth
+            variant="outlined"
+          />
+          <TextField
+            {...emailInputProps}
+            sx={{ gridColumn: { xs: "1/3", md: "initial" } }}
+            margin="dense"
+            type="email"
+            fullWidth
+            variant="outlined"
+          />
+          <TextField
+            {...phoneInputProps}
+            sx={{ gridColumn: "1/3" }}
+            margin="dense"
+            fullWidth
+            variant="outlined"
+          />
+          <TextField
+            {...messageInputProps}
+            sx={{ gridColumn: "1/3" }}
+            margin="dense"
+            fullWidth
+            variant="outlined"
+            multiline
+            rows={6}
+          />
+        </Box>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>{cancelLabel}</Button>
+        <Button onClick={onSubmit}>{submitLabel}</Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
 
 export default ContactFormDialog;
-
-const styles = stylesheet.create({
-  root: {},
-});
