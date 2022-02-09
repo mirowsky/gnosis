9ab7b34d35028e15aaea9726baba52834496ccb4
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-02-09 04:38:16 -0300

    [FEATURE] Syllabus item responsive implementation

--- components/Pages/Course/Sections/Syllabus/SyllabusItem/SyllabusItem.fixture.ts
+++ components/Pages/Course/Sections/Syllabus/SyllabusItem/SyllabusItem.fixture.ts
@@ -1,5 +1,9 @@
 import type { SyllabusItemProps } from "./SyllabusItem";
 
-export const DEFAULT_SyllabusItem_TESTING_PROPS: SyllabusItemProps = {};
+export const DEFAULT_SyllabusItem_TESTING_PROPS: SyllabusItemProps = {
+  hours: "32hrs",
+  title: "Filosofia dos valores moraise legislação médica",
+  fontSize: { xs: "1rem" },
+};
 
 export const SyllabusItem_TEST_ID = {} as const;
--- components/Pages/Course/Sections/Syllabus/SyllabusItem/SyllabusItem.stories.tsx
+++ components/Pages/Course/Sections/Syllabus/SyllabusItem/SyllabusItem.stories.tsx
@@ -3,10 +3,24 @@ import SyllabusItem from "./SyllabusItem";
 import { DEFAULT_SyllabusItem_TESTING_PROPS } from "./SyllabusItem.fixture";
 import React from "react";
 import { Story, Meta } from "@storybook/react";
+import { Box } from "@mui/material";
 
 export default {
   title: "Course/Syllabus Section/Syllabus Item",
   component: SyllabusItem,
+  parameters: {
+    layout: "fullscreen",
+    viewport: {
+      defaultViewport: "brazilDesktop1",
+    },
+  },
+  decorators: [
+    (Story) => (
+      <Box sx={{ m: 5, width: "500px" }}>
+        <Story />
+      </Box>
+    ),
+  ],
 } as Meta<SyllabusItemProps>;
 
 const Template: Story<SyllabusItemProps> = (props) => (
