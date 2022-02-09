7460b7e7b35591092f66ebd9bc21d8d05aba6176
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-02-09 20:38:28 -0300

    [ENH] Separated styled tabs to its own component

--- components/Common/StyledTabs/StyledTabs.fixture.tsx
+++ components/Common/StyledTabs/StyledTabs.fixture.tsx
@@ -1,5 +1,9 @@
 import type { StyledTabsProps } from "./StyledTabs";
 
-export const DEFAULT_StyledTabs_TESTING_PROPS: StyledTabsProps = {};
+export const DEFAULT_StyledTabs_TESTING_PROPS: StyledTabsProps = {
+  activeIndex: 0,
+  handleChange: () => {},
+  tabLabels: ["Hello"],
+};
 
 export const StyledTabs_TEST_ID = {} as const;
--- components/Common/StyledTabs/StyledTabs.tsx
+++ components/Common/StyledTabs/StyledTabs.tsx
@@ -1,9 +1,50 @@
+import { AppBar, Box, Tab, Tabs } from "@mui/material";
+import stylesheet from "@workspace/stylesheet";
 import React from "react";
 
-export type StyledTabsProps = {};
+export type StyledTabsProps = {
+  tabLabels: string[];
+  handleChange: (event: React.SyntheticEvent, newValue: number) => void;
+  activeIndex: number;
+};
 
-export const StyledTabs = (props: StyledTabsProps) => {
-  return <div>Hello, StyledTabs!</div>;
+export const StyledTabs = ({
+  activeIndex,
+  handleChange,
+  tabLabels,
+}: StyledTabsProps) => {
+  return (
+    <Box>
+      <Box component={AppBar} sx={styles.root}>
+        <Tabs
+          indicatorColor="secondary"
+          textColor="inherit"
+          value={activeIndex}
+          onChange={handleChange}
+          centered
+        >
+          {tabLabels.map((label, index) => {
+            return <Tab sx={styles.tabItem} label={label} key={index} />;
+          })}
+        </Tabs>
+      </Box>
+    </Box>
+  );
 };
 
 export default StyledTabs;
+
+const styles = stylesheet.create({
+  root: {
+    width: "100%",
+    bgcolor: (theme) => theme.palette.primary.main,
+    minHeight: "48px",
+    color: (theme) => theme.palette.primary.contrastText,
+    position: "relative",
+    borderRadius: (theme) => ({ xs: 0, sm: theme.shape.borderRadius }),
+  },
+
+  tabItem: {
+    fontSize: { xs: "0.6rem", sm: "0.75rem" },
+  },
+});
