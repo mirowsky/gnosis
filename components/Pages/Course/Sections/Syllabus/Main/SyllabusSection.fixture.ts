0f19158c4d8d543809c3a2d495773c4fb0657f0f
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-02-10 02:15:28 -0300

    [FEATURE] INitial syllabus section integration and implementation

--- components/Pages/Course/Sections/Syllabus/Main/SyllabusSection.fixture.ts
+++ components/Pages/Course/Sections/Syllabus/Main/SyllabusSection.fixture.ts
@@ -1,5 +1,32 @@
+import { DEFAULT_SyllabusItem_TESTING_PROPS } from "../SyllabusItem/SyllabusItem.fixture";
 import type { SyllabusSectionProps } from "./SyllabusSection";
 
-export const DEFAULT_SyllabusSection_TESTING_PROPS: SyllabusSectionProps = {};
+const syllabus_item = DEFAULT_SyllabusItem_TESTING_PROPS;
+
+export const DEFAULT_SyllabusSection_TESTING_PROPS: SyllabusSectionProps = {
+  activeIndex: 0,
+  handleChange: () => {},
+  syllabusItems: [
+    syllabus_item,
+    syllabus_item,
+    syllabus_item,
+    syllabus_item,
+    syllabus_item,
+    syllabus_item,
+    syllabus_item,
+  ],
+  prerequisites: "Diploma de medicina válido em território brasileiro",
+  courseType: "À partir de 600 horas",
+  emec: {
+    img: {
+      alt: "alt text, change me",
+      src: "https://via.placeholder.com/1500",
+    },
+    url: {
+      href: "https://via.placeholder",
+      label: "E-MEC link",
+    },
+  },
+};
 
 export const SyllabusSection_TEST_ID = {} as const;
--- components/Pages/Course/Sections/Syllabus/Main/SyllabusSection.tsx
+++ components/Pages/Course/Sections/Syllabus/Main/SyllabusSection.tsx
@@ -1,15 +1,116 @@
+/* eslint-disable react/jsx-key */
 import { Box } from "@mui/material";
+import {
+  TabbedContainer,
+  TabbedContainerProps,
+} from "@workspace/components/common";
 import stylesheet from "@workspace/stylesheet";
 import React from "react";
+import SyllabusItem, { SyllabusItemProps } from "../SyllabusItem/SyllabusItem";
 
-export type SyllabusSectionProps = {};
+type EMEC = {
+  img: {
+    src: string;
+    alt: string;
+  };
+  url: {
+    href: string;
+    label: string;
+  };
+};
 
-export const SyllabusSection = (props: SyllabusSectionProps) => {
-  return <Box sx={styles.root}></Box>;
+export interface SyllabusSectionProps {
+  activeIndex: number;
+  handleChange: (...args: unknown[]) => void;
+  syllabusItems: SyllabusItemProps[];
+  courseType: string;
+  prerequisites: string;
+  emec: EMEC;
+}
+
+export const SyllabusSection = ({
+  courseType,
+  emec,
+  prerequisites,
+  syllabusItems,
+  activeIndex = 0,
+  handleChange = () => {},
+}: SyllabusSectionProps) => {
+  return (
+    <Box sx={styles.root}>
+      <TabbedContainer
+        items={[
+          <SyllabusGrid items={syllabusItems} />,
+          <SyllabusCourseType type={courseType} />,
+          <SyllabusPrerequisites prerequisites={prerequisites} />,
+          <SyllabusEMEC emec={emec} />,
+        ]}
+        TabProps={{
+          activeIndex: activeIndex,
+          handleChange: handleChange,
+          centered: true,
+          tabLabels: [
+            "Matriz curricular",
+            "Modalidade",
+            "Pré-requisitos",
+            "E-MEC",
+          ],
+        }}
+      />
+    </Box>
+  );
+};
+
+const SyllabusEMEC = ({ emec }: { emec: EMEC }) => {
+  return (
+    <Box sx={styles.syllabusEMECContainer}>
+      <Box
+        sx={styles.syllabusEMECImage}
+        component="img"
+        role="img"
+        src={emec.img.src}
+        alt={emec.img.alt}
+      />
+
+      <Box component="a" href={emec.url.href} sx={styles.syllabusEMECURL}>
+        {emec.url.label}
+      </Box>
+    </Box>
+  );
+};
+
+const SyllabusPrerequisites = ({
+  prerequisites,
+}: {
+  prerequisites: string;
+}) => {
+  return <Box sx={styles.syllabusPrerequisites}>{prerequisites}</Box>;
+};
+
+const SyllabusCourseType = ({ type }: { type: string }) => {
+  return <Box sx={styles.syllabusCourseType}>{type}</Box>;
+};
+
+const SyllabusGrid = ({ items }: { items: SyllabusItemProps[] }) => {
+  return (
+    <Box sx={styles.syllabusGrid}>
+      {items.map((item, index) => {
+        <SyllabusItem {...item} key={index} />;
+      })}
+    </Box>
+  );
 };
 
 export default SyllabusSection;
 
 const styles = stylesheet.create({
   root: {},
+
+  syllabusGrid: {},
+  syllabusCourseType: {},
+  syllabusPrerequisites: {},
+
+  syllabusEMECContainer: {},
+  syllabusEMECImage: {},
+  syllabusEMECURL: {},
 });
