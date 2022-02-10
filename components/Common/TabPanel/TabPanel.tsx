b8f2a20a39a3d90f3c463865bc70f1ee4660db05
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-02-09 21:24:03 -0300

    [ENH] Transition animation at tabpanel to improve user experience

--- components/Common/TabPanel/TabPanel.tsx
+++ components/Common/TabPanel/TabPanel.tsx
@@ -1,4 +1,5 @@
 import { Box } from "@mui/material";
+import { MotionBox } from "@workspace/components/utility";
 import React from "react";
 
 export type TabPanelProps = {
@@ -14,14 +15,30 @@ export const TabPanel = ({
   ...rest
 }: TabPanelProps) => {
   return (
-    <div
+    <Box
       role="tabpanel"
       hidden={value !== index}
       id={`simple-tabpanel-${index}`}
       aria-labelledby={`simple-tab-${index}`}
     >
-      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
-    </div>
+      {value === index && (
+        <MotionBox
+          animate="visible"
+          initial="hidden"
+          variants={{
+            hidden: {
+              opacity: 0,
+            },
+            visible: {
+              opacity: 1,
+            },
+          }}
+          sx={{ p: 3 }}
+        >
+          {children}
+        </MotionBox>
+      )}
+    </Box>
   );
 };
 
