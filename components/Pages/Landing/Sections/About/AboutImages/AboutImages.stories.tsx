72afdcedcbc6f759228d62901b01a08fc7a0201e
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-02-10 21:30:07 -0300

    [REFACTOR] NEw setup for about section images that makes it easier to get responsive yet interative behavior

--- components/Pages/Landing/Sections/About/AboutImages/AboutImages.stories.tsx
+++ components/Pages/Landing/Sections/About/AboutImages/AboutImages.stories.tsx
@@ -3,6 +3,7 @@ import AboutImages from "./AboutImages";
 import { DEFAULT_AboutImages_TESTING_PROPS } from "./AboutImages.fixture";
 import React from "react";
 import { Story, Meta } from "@storybook/react";
+import { Box } from "@mui/material";
 
 export default {
   title: "Landing/About Section/About images",
@@ -13,6 +14,22 @@ export default {
       defaultViewport: "brazilDesktop1",
     },
   },
+  decorators: [
+    (Story) => (
+      <Box
+        sx={{
+          width: "100%",
+          maxWidth: "70vw",
+          margin: "auto",
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <Story />
+      </Box>
+    ),
+  ],
 } as Meta<AboutImagesProps>;
 
 const Template: Story<AboutImagesProps> = (props) => <AboutImages {...props} />;
--- components/Pages/Landing/Sections/About/AboutImages/AboutImages.tsx
+++ components/Pages/Landing/Sections/About/AboutImages/AboutImages.tsx
@@ -1,39 +1,26 @@
 import { Box } from "@mui/material";
 import stylesheet from "@workspace/stylesheet";
 import React from "react";
-import image_3 from "@workspace/images/about-us-pic-1.webp";
-import image_2 from "@workspace/images/about-us-pic-2.webp";
-import image_1 from "@workspace/images/about-us-pic-3.webp";
-
+import image from "@workspace/images/about-full.webp";
+import AboutImagesCircle from "../AboutImagesCircle/AboutImagesCircle";
 export type AboutImagesProps = {};
 
 export const AboutImages = (props: AboutImagesProps) => {
   return (
     <Box sx={styles.root}>
-      <Box sx={styles.imagesContainer}>
-        <Box sx={styles.firstImageBox}>
-          <Box
-            sx={styles.firstImage}
-            component="img"
-            src={image_1 as unknown as string}
-          />
-        </Box>
+      <Box sx={styles.rightCircleBox}>
+        <AboutImagesCircle color="primary" />
+      </Box>
+      <Box sx={styles.leftCircleBox}>
+        <AboutImagesCircle color="secondary" />
+      </Box>
 
-        <Box sx={styles.secondImageBox}>
-          <Box
-            sx={styles.secondImage}
-            component="img"
-            src={image_2 as unknown as string}
-          />
-        </Box>
-
-        <Box sx={styles.thirdImageBox}>
-          <Box
-            sx={styles.thirdImage}
-            component="img"
-            src={image_3 as unknown as string}
-          />
-        </Box>
+      <Box sx={styles.imageBox}>
+        <Box
+          sx={styles.image}
+          component="img"
+          src={image as unknown as string}
+        />
       </Box>
     </Box>
   );
@@ -44,46 +31,25 @@ export default AboutImages;
 const styles = stylesheet.create({
   root: {
     position: "relative",
-    fontSize: "1rem",
   },
 
-  imagesContainer: {
-    display: "grid",
-    gridTemplateColumns:
-      "minmax(0, 0.333fr) minmax(0, 0.333fr) minmax(0, 0.333fr)",
-    position: "relative",
-    height: { xs: "56.65em" },
-  },
-
-  firstImageBox: {
-    height: "100%",
-    position: "absolute",
-    gridColumn: "1/2",
-  },
-  secondImageBox: {
-    height: "100%",
-    position: "absolute",
-    gridColumn: "2/3",
-  },
-  thirdImageBox: {
-    height: "100%",
-    position: "absolute",
-    gridColumn: "3/4",
-  },
-
-  firstImage: {
+  imageBox: {},
+  image: {
     objectFit: "contain",
     width: "100%",
     height: "100%",
   },
-  secondImage: {
-    objectFit: "contain",
-    width: "100%",
-    height: "100%",
+
+  leftCircleBox: {
+    position: "absolute",
+    left: 0,
+    top: 0,
+    zIndex: -1,
   },
-  thirdImage: {
-    objectFit: "contain",
-    width: "100%",
-    height: "100%",
+  rightCircleBox: {
+    position: "absolute",
+    right: 0,
+    top: 0,
+    zIndex: -1,
   },
 });
