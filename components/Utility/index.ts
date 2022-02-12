0c2780794b40a2aae3fb1800794fc8bdc194be85
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-02-12 19:45:50 -0300

    [FEAT] ScrollBackTop utility component

--- components/Utility/index.ts
+++ components/Utility/index.ts
@@ -3,3 +3,5 @@ export type { SwiperGenericWrapperProps } from "./SwiperWrapper/SwiperWrapper";
 export { MotionBox } from "./MotionBox/MotionBox";
 export { HideOnScroll } from "./HideOnScroll/HideOnScroll";
 export type { HideOnScrollProps } from "./HideOnScroll/HideOnScroll";
+export { ScrollBackTop } from "./ScrollBackTop/ScrollBackTop";
+export type { ScrollBackTopProps } from "./ScrollBackTop/ScrollBackTop";
