14eb9cf430155eaca3d848bb3e2b5e3e099c2dc5
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-02-14 05:53:47 -0300

    [FEAT][ENH] Course page sections integration; prop mapping; storybook variant with layout as a decorator to visual test if the page and said layout also integrates nicely

--- components/Pages/Course/Main/CoursePage.stories.tsx
+++ components/Pages/Course/Main/CoursePage.stories.tsx
@@ -3,6 +3,8 @@ import CoursePage from "./CoursePage";
 import { DEFAULT_CoursePage_TESTING_PROPS } from "./CoursePage.fixture";
 import React from "react";
 import { Story, Meta } from "@storybook/react";
+import MainLayout from "../../../Layout/MainLayout/MainLayout";
+import { MAIN_LAYOUT_DEFAULT_PROPS } from "../../../Layout/MainLayout/constants";
 
 export default {
   title: "Course/Page/Main",
@@ -20,3 +22,19 @@ export const Primary = Template.bind({});
 Primary.args = {
   ...DEFAULT_CoursePage_TESTING_PROPS,
 };
+
+export const Secondary = Template.bind({});
+
+Secondary.args = {
+  ...Primary.args,
+};
+
+Secondary.storyName = "With Main Layout";
+
+Secondary.decorators = [
+  (Story) => (
+    <MainLayout {...MAIN_LAYOUT_DEFAULT_PROPS}>
+      <Story />
+    </MainLayout>
+  ),
+];
--- components/Pages/Course/Main/CoursePage.tsx
+++ components/Pages/Course/Main/CoursePage.tsx
@@ -1,9 +1,40 @@
+import { Box } from "@mui/material";
+import stylesheet from "@workspace/stylesheet";
 import React from "react";
+import CourseHeroSection, {
+  CourseHeroSectionProps,
+} from "../Sections/Hero/Main/CourseHeroSection";
+import OverviewSection, {
+  OverviewSectionProps,
+} from "../Sections/Overview/Main/OverviewSection";
+import SyllabusSection, {
+  SyllabusSectionProps,
+} from "../Sections/Syllabus/Main/SyllabusSection";
 
-export type CoursePageProps = {};
+export type CoursePageProps = {
+  CourseHeroSectionProps: CourseHeroSectionProps;
+  CourseOverviewSectionProps: OverviewSectionProps;
+  CourseSyllabusSectionProps: SyllabusSectionProps;
+};
 
-export const CoursePage = (props: CoursePageProps) => {
-  return <div>Hello, CoursePage!</div>;
+export const CoursePage = ({
+  CourseHeroSectionProps,
+  CourseOverviewSectionProps,
+  CourseSyllabusSectionProps,
+}: CoursePageProps) => {
+  return (
+    <Box sx={styles.root}>
+      <CourseHeroSection {...CourseHeroSectionProps} />
+
+      <OverviewSection {...CourseOverviewSectionProps} />
+
+      <SyllabusSection {...CourseSyllabusSectionProps} />
+    </Box>
+  );
 };
 
 export default CoursePage;
+
+const styles = stylesheet.create({
+  root: {},
+});
