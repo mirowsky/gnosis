52a1548c11814afffbb229ab0341d3ebf779d2cf
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-02-18 18:57:14 -0300

    [FEATURE] INitial API implementation for SlideIn animation

--- animations/SlideIn/SlideIn.tsx
+++ animations/SlideIn/SlideIn.tsx
@@ -1,14 +1,47 @@
+import { MotionBox } from "@workspace/components/utility";
 import stylesheet from "@workspace/stylesheet";
+import { ThemeStyles } from "@workspace/types";
+import { Variants } from "framer-motion";
 import React from "react";
 
-export type SlideInProps = {};
+export type SlideInProps = {
+  children?: React.ReactNode;
+  animate?: boolean;
+  sx?: ThemeStyles;
+  direction?: "left" | "right" | "up" | "down";
+};
 
-export const SlideIn = (props: SlideInProps) => {
-  return <div>Hello, SlideIn!</div>;
+export const SlideIn = ({
+  animate,
+  children,
+  sx,
+  direction = "up",
+}: SlideInProps) => {
+  return <MotionBox sx={{ ...styles.root, ...sx }}>{children}</MotionBox>;
 };
 
 export default SlideIn;
 
-const style = stylesheet.create({
+const styles = stylesheet.create({
   root: {},
+});
+
+const ANIMATION_VARIANTS = (
+  direction: SlideInProps["direction"] = "up"
+): Variants => ({
+  initial: {
+    ...(direction === "up"
+      ? {
+          y: -250,
+        }
+      : direction === "down"
+      ? { y: 250 }
+      : direction === "left"
+      ? { x: 250 }
+      : { x: -250 }),
+  },
+
+  finished: {
+    ...(direction === "up" || direction === "down" ? { y: 0 } : { x: 0 }),
+  },
 });
