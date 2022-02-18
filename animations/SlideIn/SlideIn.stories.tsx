e5ef0f69eadca92af77f89cd8132ede88a40df81
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-02-18 19:07:39 -0300

    [CONFIG] SlideIn story with custom demo setup

--- animations/SlideIn/SlideIn.stories.tsx
+++ animations/SlideIn/SlideIn.stories.tsx
@@ -1,16 +1,37 @@
-import type { SlideInProps } from "./SlideIn";
 import SlideIn from "./SlideIn";
-import { DEFAULT_SlideIn_TESTING_PROPS } from "./SlideIn.fixture";
 import React from "react";
 import { Story, Meta } from "@storybook/react";
+import { Box } from "@mui/material";
+
+type SlideInDemoProps = {
+  animate?: boolean;
+};
+
+const SlideInDemo = ({ animate }: SlideInDemoProps) => {
+  return (
+    <SlideIn animate={animate}>
+      <Box
+        // eslint-disable-next-line react/no-children-prop
+        children="Im a box"
+        sx={{
+          bgcolor: "violet",
+          borderRadius: "12px",
+          w: "350px",
+          h: "350px",
+          boxShadow: (theme) => theme.shadows[5],
+        }}
+      />
+    </SlideIn>
+  );
+};
 
 export default {
   title: "Animations/Slide in",
-  component: SlideIn,
-} as Meta<SlideInProps>;
+  component: SlideInDemo,
+} as Meta<SlideInDemoProps>;
 
-const Template: Story<SlideInProps> = (props) => <SlideIn {...props} />;
+const Template: Story<SlideInDemoProps> = (props) => <SlideInDemo {...props} />;
 export const Primary = Template.bind({});
 Primary.args = {
-  ...DEFAULT_SlideIn_TESTING_PROPS,
+  animate: true,
 };
