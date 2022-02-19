af0fcbd67d9c68db6476615e76210bb951dcc622
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-02-19 04:01:57 -0300

    [FEATURE] Initial implementation for slidegroupinonscroll

--- animations/SlideGroupInOnScroll/SlideGroupInOnScroll.stories.tsx
+++ animations/SlideGroupInOnScroll/SlideGroupInOnScroll.stories.tsx
@@ -4,19 +4,32 @@ import { DEFAULT_SlideGroupInOnScroll_TESTING_PROPS } from "./SlideGroupInOnScro
 import React from "react";
 import { Story, Meta } from "@storybook/react";
 
+type SlideGroupInOnScrollDemoProps = {};
+
+const SlideGroupInOnScrollDemo = (props: SlideGroupInOnScrollDemoProps) => {
+  return (
+    <SlideGroupInOnScroll>
+      <div>hello</div>
+      <div>hello</div>
+      <div>hello</div>
+      <div>hello</div>
+    </SlideGroupInOnScroll>
+  );
+};
+
 export default {
   title: "Animations/Slide Group In On Scroll/Main",
-  component: SlideGroupInOnScroll,
+  component: SlideGroupInOnScrollDemo,
   parameters: {
     layout: "fullscreen",
     viewport: {
       defaultViewport: "brazilDesktop1",
     },
   },
-} as Meta<SlideGroupInOnScrollProps>;
+} as Meta<SlideGroupInOnScrollDemoProps>;
 
-const Template: Story<SlideGroupInOnScrollProps> = (props) => (
-  <SlideGroupInOnScroll {...props} />
+const Template: Story<SlideGroupInOnScrollDemoProps> = (props) => (
+  <SlideGroupInOnScrollDemo {...props} />
 );
 export const Primary = Template.bind({});
 Primary.args = {
--- animations/SlideGroupInOnScroll/SlideGroupInOnScroll.tsx
+++ animations/SlideGroupInOnScroll/SlideGroupInOnScroll.tsx
@@ -1,9 +1,29 @@
+import { MotionBox } from "@workspace/components/utility";
+import { ThemeStyles } from "@workspace/types";
 import React from "react";
 
-export type SlideGroupInOnScrollProps = {};
+export type SlideGroupInOnScrollProps = {
+  children?: React.ReactNode;
+  sx?: ThemeStyles;
+};
 
-export const SlideGroupInOnScroll = (props: SlideGroupInOnScrollProps) => {
-  return <div>Hello, SlideGroupInOnScroll!</div>;
+export const SlideGroupInOnScroll = ({
+  children,
+  sx,
+}: SlideGroupInOnScrollProps) => {
+  return (
+    <MotionBox sx={{ ...sx }}>
+      {children &&
+        React.Children.map(children, (child) => (
+          <MotionBox>
+            {React.cloneElement(
+              child as React.ReactElement<unknown>,
+              {} as typeof MotionBox["defaultProps"]
+            )}
+          </MotionBox>
+        ))}
+    </MotionBox>
+  );
 };
 
 export default SlideGroupInOnScroll;
