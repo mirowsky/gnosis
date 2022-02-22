6c936f47aa52e93d1721ebe25d50b8addef2ec7e
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-02-22 03:02:55 -0300

    [ENH][REFACTOR] Attempt optimization of props evaluation at nextjs _app file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,7 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import * as Yup from "yup";
 import { Money, OnlinePredictionTwoTone, School } from "@mui/icons-material";
 import { handleLogoClick, handleMenuClick } from "@workspace/utility";
+import { GlobalSnack } from "@workspace/components/utility";
 
 type ContactFormInputs = {
   name: string;
@@ -61,147 +62,196 @@ function MyApp(props: AppProps & { emotionCache?: EmotionCache }) {
     ),
   });
 
+  const contactDialProps: MainLayoutProps["ContactDialProps"] =
+    React.useMemo(() => {
+      return {
+        ContactDialButtonProps: {
+          open: contactDialOpen,
+          onClick: () => setContactDialOpen((prevState) => !prevState),
+        },
+        items: [
+          { action: () => {}, icon: Money, label: "Valores" },
+          { action: () => {}, icon: School, label: "Validação de diploma" },
+          {
+            action: () => {},
+            icon: OnlinePredictionTwoTone,
+            label: "Sistema de aprendizado virtual",
+          },
+        ],
+      };
+    }, [contactDialOpen]);
+
+  const headerProps: MainLayoutProps["HeaderProps"] = React.useMemo(() => {
+    return {
+      logo: {
+        src: "/images/logo.svg",
+        alt: "Instituto Educacional Gnosis - Logotipo - A figura de uma coruja formado por polígonos",
+        label: "Instituto Gnosis",
+      },
+      items: [
+        {
+          label: "Sobre nós",
+          onClick: () => handleMenuClick(router, "#about_us_section"),
+        },
+        {
+          label: "Cursos",
+          onClick: () => handleMenuClick(router, "#course_section"),
+        },
+      ],
+      onLogoClick: () => handleLogoClick(router),
+      cta: {
+        label: "Contate-nos",
+        onClick: () => setContactFormDialogOpen(true),
+      },
+      burguerOpen: mobileMenuOpen,
+      onBurguerButtonClick: () => setMobileMenuOpen(true),
+    };
+  }, [mobileMenuOpen, router]);
+
+  const footerProps: MainLayoutProps["FooterProps"] = React.useMemo(() => {
+    return {
+      FooterContactInfoProps: {
+        address: "Avenida Praia de Belas, 1212, sala 424, RS - Brasil",
+        emails: ["atendimento@institutoeg.com"],
+        phones: ["(51) 9-9143-1009"],
+        registration: "CNPJ: 36.605.433/0001-18",
+      },
+      FooterIconsProps: {
+        facebook: "https://www.facebook.com/InstitutoGnosisEducacional/",
+        instagram: "https://www.instagram.com/gnosisinstitutoeducacional/",
+        whatsApp: "https://wa.link/3in1jl",
+        twitter: "https://twitter.com/GnosisInstituto",
+        linkedin:
+          "https://www.youtube.com/channel/UC4qx3U-Hk8qlX46zTZqrL0w/videos?view_as=subscriber",
+        youtube:
+          "https://www.youtube.com/channel/UC4qx3U-Hk8qlX46zTZqrL0w/videos?view_as=subscriber",
+      },
+    };
+  }, []);
+
+  const contactSectionProps: MainLayoutProps["ContactSectionProps"] = {
+    id: "contact_section",
+    img: {
+      src: "/images/contact-doc.webp",
+      alt: "Doutoura jovem, vestindo um jaleco e com um estetoscópio em volta de seu pescoço, apontando para a esquerda com um de seus dedos",
+    },
+    ContactFormProps: {
+      LoadingButtonProps: {
+        loading: contactForm.formState.isSubmitting,
+        disabled: !contactForm.formState.isValid,
+        children: "Enviar",
+      },
+      EmailInputProps: {
+        ...contactForm.register("email"),
+        label: "E-mail",
+        placeholder: "Ex: joao.alves@gmail.com",
+        error: Boolean(contactForm.formState.errors.email),
+        helperText: contactForm.formState.errors.email?.message || " ",
+      },
+      MessageIputProps: {
+        ...contactForm.register("message"),
+        label: "Mensagem",
+        placeholder: "Ex: Gostaria de saber como funciona o curso de...",
+        error: Boolean(contactForm.formState.errors.message),
+        helperText: contactForm.formState.errors.message?.message || " ",
+      },
+      NameInputProps: {
+        ...contactForm.register("name"),
+        label: "Nome completo",
+        placeholder: "Ex: João Alves da Silva",
+        error: Boolean(contactForm.formState.errors.name),
+        helperText: contactForm.formState.errors.name?.message || " ",
+      },
+      PhoneInputProps: {
+        ...contactForm.register("phone"),
+        onValueChange: ({
+          floatValue: _floatValue,
+          formattedValue,
+          value: _value,
+        }) => {
+          contactForm.setValue("phone", formattedValue);
+        },
+        label: "Número de telefone",
+        placeholder: "Ex: (99) 9-8765-4321",
+        error: Boolean(contactForm.formState.errors.phone),
+        helperText: contactForm.formState.errors.phone?.message || " ",
+      },
+    },
+    sectionTitle: "Solicite sua matrícula ou tire dúvidas sobre nossos cursos.",
+  };
+
+  const newsLetterSectionProps: MainLayoutProps["NewsLetterSectionProps"] = {
+    img: {
+      src: "/images/newsletter-doc.webp",
+      alt: "Médica jovem, sorrindo para a câmera, vestindo um jaleco, com os dois braços em frente ao seu corpo",
+    },
+    sectionTitle: "Assine nossa Newsletter e fique por dentro das novidades.",
+    InputWithButtonProps: {
+      InputProps: {
+        ...newsletterInput.register("email"),
+        placeholder: "Digite seu e-mail aqui",
+      },
+      ButtonProps: {
+        loading: newsletterInput.formState.isSubmitting,
+        disabled: !newsletterInput.formState.isValid,
+      },
+    },
+  };
+  const contactFormDialogProps: MainLayoutProps["ContactFormDialogProps"] = {
+    cancelLabel: "Cancelar",
+    title: "Contato",
+    subtitle:
+      "Preencha o formulário com seus dados e uma mensagem e um de nossos representantes irá atendê-lo na primeira oportunidade.",
+    submitLabel: "Enviar",
+    open: contactFormDialogOpen,
+    handleClose: () => setContactFormDialogOpen(false),
+    emailInputProps: {
+      label: "E-mail",
+      placeholder: "Ex: joao.alves@gmail.com",
+      ...dialogForm.register("email"),
+    },
+    messageInputProps: {
+      label: "Mensagem",
+      placeholder: "Ex: Gostaria de saber como funciona o curso de...",
+      ...dialogForm.register("message"),
+    },
+    nameInputProps: {
+      label: "Nome completo",
+      placeholder: "Ex: João Alves da Silva",
+      ...dialogForm.register("name"),
+    },
+    phoneInputProps: {
+      onValueChange: (values) => {
+        dialogForm.setValue("phone", values.formattedValue);
+      },
+      label: "Número de telefone",
+      placeholder: "Ex: (99) 9-8765-4321",
+      ...dialogForm.register("phone"),
+    },
+  };
+
+  const mobileMenuProps: MainLayoutProps["MobileMenuProps"] = {
+    menuItems: headerProps.items,
+    logo: headerProps.logo,
+    open: mobileMenuOpen,
+    onOpen: () => setMobileMenuOpen(true),
+    onClose: () => setMobileMenuOpen(false),
+  };
+
   return (
     <React.Fragment>
       <CacheProvider value={emotionCache}>
         <CustomTheme>
           <MainLayout
-            HeaderProps={{
-              ...HEADER_PROPS,
-              items: [
-                {
-                  label: "Sobre nós",
-                  onClick: () => handleMenuClick(router, "#about_us_section"),
-                },
-                {
-                  label: "Cursos",
-                  onClick: () => handleMenuClick(router, "#course_section"),
-                },
-              ],
-              onLogoClick: () => handleLogoClick(router),
-              cta: {
-                ...HEADER_PROPS.cta,
-                onClick: () => setContactFormDialogOpen(true),
-              },
-              burguerOpen: mobileMenuOpen,
-              onBurguerButtonClick: () => setMobileMenuOpen(true),
-            }}
-            FooterProps={FOOTER_PROPS}
-            MobileMenuProps={{
-              ...MOBILE_MENU_PROPS,
-              menuItems: [
-                {
-                  label: "Sobre nós",
-                  onClick: () => handleMenuClick(router, "#about_us_section"),
-                },
-                {
-                  label: "Cursos",
-                  onClick: () => handleMenuClick(router, "#course_section"),
-                },
-              ],
-              open: mobileMenuOpen,
-              onOpen: () => setMobileMenuOpen(true),
-              onClose: () => setMobileMenuOpen(false),
-            }}
-            ContactSectionProps={{
-              ...CONTACT_SECTION_PROPS,
-              ContactFormProps: {
-                LoadingButtonProps: {
-                  loading: contactForm.formState.isSubmitting,
-                  disabled: !contactForm.formState.isValid,
-                  children: "Enviar",
-                },
-                EmailInputProps: {
-                  ...contactForm.register("email"),
-                  label: "E-mail",
-                  placeholder: "Ex: joao.alves@gmail.com",
-                  error: Boolean(contactForm.formState.errors.email),
-                  helperText:
-                    contactForm.formState.errors.email?.message || " ",
-                },
-                MessageIputProps: {
-                  ...contactForm.register("message"),
-                  label: "Mensagem",
-                  placeholder:
-                    "Ex: Gostaria de saber como funciona o curso de...",
-                  error: Boolean(contactForm.formState.errors.message),
-                  helperText:
-                    contactForm.formState.errors.message?.message || " ",
-                },
-                NameInputProps: {
-                  ...contactForm.register("name"),
-                  label: "Nome completo",
-                  placeholder: "Ex: João Alves da Silva",
-                  error: Boolean(contactForm.formState.errors.name),
-                  helperText: contactForm.formState.errors.name?.message || " ",
-                },
-                PhoneInputProps: {
-                  ...contactForm.register("phone"),
-                  onValueChange: ({
-                    floatValue: _floatValue,
-                    formattedValue,
-                    value: _value,
-                  }) => {
-                    contactForm.setValue("phone", formattedValue);
-                  },
-                  label: "Número de telefone",
-                  placeholder: "Ex: (99) 9-8765-4321",
-                  error: Boolean(contactForm.formState.errors.phone),
-                  helperText:
-                    contactForm.formState.errors.phone?.message || " ",
-                },
-              },
-            }}
-            NewsLetterSectionProps={{
-              ...NEWSLETTER_SECTION_PROPS,
-              InputWithButtonProps: {
-                InputProps: {
-                  ...newsletterInput.register("email"),
-                  placeholder: "Digite seu e-mail aqui",
-                },
-                ButtonProps: {
-                  loading: newsletterInput.formState.isSubmitting,
-                  disabled: !newsletterInput.formState.isValid,
-                },
-              },
-            }}
-            ContactDialProps={{
-              ...CONTACT_DIAL_PROPS,
-              ContactDialButtonProps: {
-                open: contactDialOpen,
-                onClick: () => setContactDialOpen((prevState) => !prevState),
-              },
-            }}
-            ContactFormDialogProps={{
-              ...CONTACT_FORM_DIALOG_PROPS,
-              open: contactFormDialogOpen,
-              handleClose: () => setContactFormDialogOpen(false),
-              emailInputProps: {
-                label: "E-mail",
-                placeholder: "Ex: joao.alves@gmail.com",
-                ...dialogForm.register("email"),
-              },
-              messageInputProps: {
-                label: "Mensagem",
-                placeholder:
-                  "Ex: Gostaria de saber como funciona o curso de...",
-                ...dialogForm.register("message"),
-              },
-              nameInputProps: {
-                label: "Nome completo",
-                placeholder: "Ex: João Alves da Silva",
-                ...dialogForm.register("name"),
-              },
-              phoneInputProps: {
-                onValueChange: (values) => {
-                  dialogForm.setValue("phone", values.formattedValue);
-                },
-                label: "Número de telefone",
-                placeholder: "Ex: (99) 9-8765-4321",
-                ...dialogForm.register("phone"),
-              },
-            }}
+            HeaderProps={headerProps}
+            FooterProps={footerProps}
+            MobileMenuProps={mobileMenuProps}
+            ContactSectionProps={contactSectionProps}
+            NewsLetterSectionProps={newsLetterSectionProps}
+            ContactDialProps={contactDialProps}
+            ContactFormDialogProps={contactFormDialogProps}
           >
+            <GlobalSnack />
             <Component {...pageProps} />
           </MainLayout>
         </CustomTheme>
@@ -211,87 +261,3 @@ function MyApp(props: AppProps & { emotionCache?: EmotionCache }) {
 }
 
 export default MyApp;
-
-const CONTACT_FORM_DIALOG_PROPS: MainLayoutProps["ContactFormDialogProps"] = {
-  cancelLabel: "Cancelar",
-  title: "Contato",
-  subtitle:
-    "Preencha o formulário com seus dados e uma mensagem e um de nossos representantes irá atendê-lo na primeira oportunidade.",
-  submitLabel: "Enviar",
-};
-
-const CONTACT_DIAL_PROPS: MainLayoutProps["ContactDialProps"] = {
-  ContactDialButtonProps: {
-    color: "primary",
-    open: false,
-  },
-  items: [
-    { action: () => {}, icon: Money, label: "Valores" },
-    { action: () => {}, icon: School, label: "Validação de diploma" },
-    {
-      action: () => {},
-      icon: OnlinePredictionTwoTone,
-      label: "Sistema de aprendizado virtual",
-    },
-  ],
-};
-
-const NEWSLETTER_SECTION_PROPS: MainLayoutProps["NewsLetterSectionProps"] = {
-  img: {
-    src: "/images/newsletter-doc.webp",
-    alt: "Médica jovem, sorrindo para a câmera, vestindo um jaleco, com os dois braços em frente ao seu corpo",
-  },
-  sectionTitle: "Assine nossa Newsletter e fique por dentro das novidades.",
-  InputWithButtonProps: {},
-};
-
-const CONTACT_SECTION_PROPS: MainLayoutProps["ContactSectionProps"] = {
-  id: "contact_section",
-  img: {
-    src: "/images/contact-doc.webp",
-    alt: "Doutoura jovem, vestindo um jaleco e com um estetoscópio em volta de seu pescoço, apontando para a esquerda com um de seus dedos",
-  },
-  ContactFormProps: {},
-  sectionTitle: "Solicite sua matrícula ou tire dúvidas sobre nossos cursos.",
-};
-
-const FOOTER_PROPS: MainLayoutProps["FooterProps"] = {
-  FooterContactInfoProps: {
-    address: "Avenida Praia de Belas, 1212, sala 424, RS - Brasil",
-    emails: ["atendimento@institutoeg.com"],
-    phones: ["(51) 9-9143-1009"],
-    registration: "CNPJ: 36.605.433/0001-18",
-  },
-  FooterIconsProps: {
-    facebook: "https://www.facebook.com/InstitutoGnosisEducacional/",
-    instagram: "https://www.instagram.com/gnosisinstitutoeducacional/",
-    whatsApp: "https://wa.link/3in1jl",
-    twitter: "https://twitter.com/GnosisInstituto",
-    linkedin:
-      "https://www.youtube.com/channel/UC4qx3U-Hk8qlX46zTZqrL0w/videos?view_as=subscriber",
-    youtube:
-      "https://www.youtube.com/channel/UC4qx3U-Hk8qlX46zTZqrL0w/videos?view_as=subscriber",
-  },
-};
-
-const HEADER_PROPS: MainLayoutProps["HeaderProps"] = {
-  cta: {
-    label: "Contate-nos",
-    onClick: () => {},
-  },
-  burguerOpen: false,
-  items: [],
-  logo: {
-    src: "/images/logo.svg",
-    alt: "Instituto Educacional Gnosis - Logotipo - A figura de uma coruja formado por polígonos",
-    label: "Instituto Gnosis",
-  },
-};
-
-const MOBILE_MENU_PROPS: MainLayoutProps["MobileMenuProps"] = {
-  menuItems: HEADER_PROPS.items,
-  onClose: () => {},
-  onOpen: () => {},
-  logo: HEADER_PROPS.logo,
-  open: false,
-};
