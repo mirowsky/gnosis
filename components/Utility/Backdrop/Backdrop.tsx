2ac4d349967e6004580f4f683381c0e0f18073a0
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-02-24 03:28:02 -0300

    [FEATURE] Backdrop initial implementation

--- components/Utility/Backdrop/Backdrop.tsx
+++ components/Utility/Backdrop/Backdrop.tsx
@@ -1,9 +1,35 @@
+import stylesheet from "@workspace/stylesheet";
+import { ThemeStyles } from "@workspace/types";
 import React from "react";
+import { MotionBox } from "..";
 
-export type BackdropProps = {};
+export type BackdropProps = {
+  children?: React.ReactNode;
+  sx?: ThemeStyles;
+  open?: boolean;
+};
 
-export const Backdrop = (props: BackdropProps) => {
-  return <div>Hello, Backdrop!</div>;
+export const Backdrop = ({ children, sx, open }: BackdropProps) => {
+  return (
+    <MotionBox
+      initial={{ opacity: 0 }}
+      animate={open ? { opacity: 1 } : { opacity: 0 }}
+      sx={{ ...styles.root, ...sx }}
+    >
+      {children}
+    </MotionBox>
+  );
 };
 
 export default Backdrop;
+
+const styles = stylesheet.create({
+  root: {
+    width: "100vw",
+    height: "100vh",
+    position: "fixed",
+    top: 0,
+    left: 0,
+    backgroundColor: "rgba(255, 255, 255, 0.5)",
+  },
+});
