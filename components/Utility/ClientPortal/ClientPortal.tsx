e00f0dfd3ea5fd275adccc2c0a8cee13e12aa1da
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-02-24 04:19:53 -0300

    [FEATURE] ClientPortal utility component initial implementation

--- components/Utility/ClientPortal/ClientPortal.tsx
+++ components/Utility/ClientPortal/ClientPortal.tsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useRef } from "react";
+import { createPortal } from "react-dom";
 
-export type ClientPortalProps = {};
+export type ClientPortalProps = {
+  children?: React.ReactNode;
+  selector?: string;
+};
 
-export const ClientPortal = (props: ClientPortalProps) => {
-  return <div>Hello, ClientPortal!</div>;
+export const ClientPortal = ({
+  selector = "body",
+  children,
+}: ClientPortalProps) => {
+  const ref = useRef<Element>();
+  const [mounted, setMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    ref.current = document.querySelector(selector)!;
+    setMounted(true);
+  }, [selector]);
+
+  return mounted ? createPortal(children, ref.current!) : null;
 };
 
 export default ClientPortal;
