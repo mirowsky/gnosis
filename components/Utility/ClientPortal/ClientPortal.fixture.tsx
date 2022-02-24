3751b412f16da7450e91f3727fd7bc89953cde5b
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-02-24 05:01:11 -0300

    [MISC] Default props for testing

--- components/Utility/ClientPortal/ClientPortal.fixture.tsx
+++ components/Utility/ClientPortal/ClientPortal.fixture.tsx
@@ -1,5 +1,8 @@
 import type { ClientPortalProps } from "./ClientPortal";
 
-export const DEFAULT_ClientPortal_TESTING_PROPS: ClientPortalProps = {};
+export const DEFAULT_ClientPortal_TESTING_PROPS: ClientPortalProps = {
+  children: <div>I came through a portal</div>,
+  selector: "#root",
+};
 
 export const ClientPortal_TEST_ID = {} as const;
