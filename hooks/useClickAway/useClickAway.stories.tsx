42205685da60b9a35ea2ecec1db675cc72148da8
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-02-24 02:25:41 -0300

    [ENH] Imrpoved useClickAway documentation

--- hooks/useClickAway/useClickAway.stories.tsx
+++ hooks/useClickAway/useClickAway.stories.tsx
@@ -1,43 +1,80 @@
 import { Meta, Story } from "@storybook/react";
 import { Box } from "@mui/material";
 import { useClickAway } from "./useClickAway";
+import React from "react";
 
 type UseClickAwayDemoProps = {};
 
 const UseClickAwayDemo = (props: UseClickAwayDemoProps) => {
   const { ref } = useClickAway((event) => {
-    console.log("Something else was clicked inside the component tree");
+    setOutside(true);
+
+    setTimeout(() => {
+      setOutside(false);
+    }, 1750);
   });
 
+  const [outside, setOutside] = React.useState(false);
+
   return (
-    <Box
-      ref={ref}
-      component="div"
-      sx={{
-        justifyContent: "center",
-        alignItems: "center",
-        display: "flex",
-        height: "250px",
-        width: "250px",
-        bgcolor: "violet",
-        borderRadius: "12px",
-      }}
-    >
+    <React.Fragment>
       <Box
         sx={{
-          width: "50px",
-          height: "50px",
-          bgcolor: "azure",
-          borderRadius: "12px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          textAlign: "center",
+          gap: "1rem",
         }}
-      ></Box>
-    </Box>
+      >
+        <Box
+          sx={{
+            fontSize: "2rem",
+            fontWeight: 800,
+            color: outside ? "red" : "#333",
+          }}
+        >
+          {outside
+            ? "You clicked outside the component"
+            : "There was no click outside the component recently"}
+        </Box>
+
+        <Box
+          ref={ref}
+          component="div"
+          sx={{
+            justifyContent: "center",
+            alignItems: "center",
+            display: "flex",
+            height: "250px",
+            width: "250px",
+            bgcolor: "violet",
+            borderRadius: "12px",
+          }}
+        >
+          <Box
+            sx={{
+              width: "50px",
+              height: "50px",
+              bgcolor: "azure",
+              borderRadius: "12px",
+            }}
+          ></Box>
+        </Box>
+      </Box>
+    </React.Fragment>
   );
 };
 
 export default {
   title: "Hooks/useClickAway",
   component: UseClickAwayDemo,
+  parameters: {
+    viewport: {
+      defaultViewport: "brazilDesktop1",
+    },
+  },
 } as Meta<UseClickAwayDemoProps>;
 
 const Template: Story<UseClickAwayDemoProps> = (props) => (
