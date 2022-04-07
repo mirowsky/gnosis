ba970206e2f35e35cf09d4b91e2f042c459772d4
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-04-07 19:48:26 -0300

    comment typo

--- hooks/useClickAway/useClickAway.tsx
+++ hooks/useClickAway/useClickAway.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const useClickAway = (onClickAway: (...args: unknown[]) => void) => {
   const ref = React.useRef<HTMLElement | null>(null);
 
-  // Favoring this approach instead of just using a React.useCallback to prevent having to add a dependency to the second useEffect that attaches an event listener to the document, kaing it stable and preventing it from having to destruct and attach it again render
+  // Favoring this approach instead of just using a React.useCallback to prevent having to add a dependency to the second useEffect that attaches an event listener to the document, keeping it stable and preventing it from having to destruct and attach it again render
   const callback = React.useRef(onClickAway);
 
   React.useEffect(() => {
