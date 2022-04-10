66d13b9c7a4b4a915a3e93674def3e7c0dfff5df
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-04-10 20:35:32 -0300

    pattern detail to enhance FAQ section visuals

--- components/Layout/DynamicSections/DynamicSections.tsx
+++ components/Layout/DynamicSections/DynamicSections.tsx
@@ -33,10 +33,29 @@ export const DynamicSections = ({
         }}
       />
 
-      <FAQSection
-        {...FAQSectionProps}
-        sx={{ py: (theme) => ({ xs: theme.spacing(10) }) }}
-      />
+      <Box sx={{ position: "relative" }}>
+        <FAQSection
+          {...FAQSectionProps}
+          sx={{ py: (theme) => ({ xs: theme.spacing(10) }) }}
+        />
+        <Box
+          sx={{
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+            backgroundColor: "#F6F9FB",
+            top: 0,
+            left: 0,
+            opacity: 0.05,
+            zIndex: -1,
+            backgroundImage:
+              "linear-gradient(#002946 1.6px, transparent 1.6px), linear-gradient(90deg, #002946 1.6px, transparent 1.6px), linear-gradient(#002946 0.8px, transparent 0.8px), linear-gradient(90deg, #002946 0.8px, #F6F9FB 0.8px)",
+            backgroundSize: "40px 40px, 40px 40px, 8px 8px, 8px 8px",
+            backgroundPosition:
+              "-1.6px -1.6px, -1.6px -1.6px, -0.8px -0.8px, -0.8px -0.8px",
+          }}
+        />
+      </Box>
     </Box>
   );
 };
@@ -47,6 +66,5 @@ const styles = stylesheet.create({
   root: {
     display: "flex",
     flexDirection: "column",
-    gap: { xs: "6rem" },
   },
 });
