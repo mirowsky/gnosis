de41d325fc316287df9cc4802a421c69724a0b5c
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-04-10 16:03:14 -0300

    moved blog handler to its own file to improve file structure and void tighly coupling

---
+++ mocks/handlers/blog.ts
@@ -0,0 +1,27 @@
+import faker from "@faker-js/faker";
+import { COLLECTIONS_API_ROUTES } from "../../constants";
+import { rest } from "msw";
+
+//fetching after the "window" object is available will be mocked by "mocks/browser.js", fetching before it will be mocked by "mocks/server.js"
+
+const randomBlogPost = (): import("../../types").BlogCollectionType => ({
+  blogActive: true,
+  blogDescription: faker.lorem.lines(5),
+  blogPost: faker.lorem.paragraphs(10),
+  blogTitle: faker.lorem.words(3),
+  featuredImage: {
+    imageURL: "https://source.unsplash.com/random",
+    imageDescription: faker.lorem.sentences(2),
+  },
+  slug: "blog-post-1",
+  uuid: faker.datatype.uuid(),
+});
+
+export const blogHandler = rest.get(
+  COLLECTIONS_API_ROUTES.blog,
+  (req, res, ctx) => {
+    return res(
+      ctx.json([randomBlogPost(), randomBlogPost(), randomBlogPost()])
+    );
+  }
+);
