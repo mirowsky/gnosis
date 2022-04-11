c574869d578533051c18d9b3aad9cf6e98c7cf34
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-04-11 00:50:41 -0300

    created and exposed a singleton for every firestore collection service available

--- services/index.ts
+++ services/index.ts
@@ -1,51 +1,10 @@
-import {
-  BlogCollectionType,
-  CourseCollectionType,
-  FAQCollectionType,
-  TestimonialCollectionType,
-} from "@workspace/types";
-import { httpGet } from "@workspace/utility";
+import { FirebaseCollectionFactory } from "./collections";
 
-interface IQueryable<T> {
-  findOne: (id: string) => Promise<T>;
-  findAll: () => Promise<T[]>;
-}
+const factory = new FirebaseCollectionFactory();
 
-class FirebaseService<T> implements IQueryable<T> {
-  private readonly baseRoute =
-    "https://us-central1-gnosis-webapp.cloudfunctions.net/api/collections/entries/";
+const CoursesService = factory.createCourses();
+const BlogService = factory.createBlog();
+const FAQService = factory.createFAQ();
+const TestimonialService = factory.createTestimonial();
 
-  private readonly apiRoute: string;
-
-  constructor(collection: string) {
-    this.apiRoute = `${this.baseRoute}${collection}`;
-  }
-
-  findAll = async (): Promise<T[]> => {
-    return await httpGet(`${this.apiRoute}`);
-  };
-
-  findOne = async (id: string): Promise<T> => {
-    return await httpGet(`${this.apiRoute}/${id}`);
-  };
-}
-
-interface IFirebaseCollectionFactory {
-  createCourses: () => FirebaseService<CourseCollectionType>;
-  createBlog: () => FirebaseService<BlogCollectionType>;
-  createTestimonial: () => FirebaseService<TestimonialCollectionType>;
-  createFAQ: () => FirebaseService<FAQCollectionType>;
-}
-
-export class FirebaseCollectionFactory implements IFirebaseCollectionFactory {
-  createBlog = () => {
-    return new FirebaseService<BlogCollectionType>("gnosisBlog");
-  };
-
-  createCourses = () => new FirebaseService<CourseCollectionType>("coursesNew");
-
-  createFAQ = () => new FirebaseService<FAQCollectionType>("faq");
-
-  createTestimonial = () =>
-    new FirebaseService<TestimonialCollectionType>("testimonials");
-}
+export { CoursesService, BlogService, FAQService, TestimonialService };
