ddc4c98904cee6af38982e7b3c3e3933b1919fa0
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-04-13 22:47:43 -0300

    static props for about section proxy

--- src/features/home/screens/MainV2/About.proxy.tsx
+++ src/features/home/screens/MainV2/About.proxy.tsx
@@ -1,7 +1,14 @@
 import { AboutSection, AboutSectionProps } from "../About/AboutSection";
 
-export type AboutProxyProps = {} & AboutSectionProps;
+export type AboutProxyProps = {};
 
 export const AboutProxy = (props: AboutProxyProps) => {
-  return <AboutSection {...props} />;
+  return <AboutSection {...PROPS} />;
+};
+
+const PROPS = {
+  id: "about_us_section",
+  aboutText:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce in enim vitae augue facilisis semper ut eget ligula. Vestibulum feugiat congue odio ut pretium. Praesent velit sapien, aliquet eu ipsum faucibus, malesuada vehicula neque. Pellentesque et blandit est. Proin aliquet orci sapien, quis semper tortor vehicula ullamcorper. Aliquam pellentesque",
+  sectionTitle: "O Instituto Gnosis",
 };
