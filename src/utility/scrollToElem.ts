f84a0ba29e6c2da2f9130bfb45444fd39fe930bf
Aleksander Mirowsky <alex.xande10@gmail.com>
2022-05-08 19:50:46 -0300

    changed package manager

--- src/utility/scrollToElem.ts
+++ src/utility/scrollToElem.ts
@@ -2,14 +2,13 @@ export const nativeSmoothScrollTo = (elem: Element | HTMLElement) => {
   window.scroll({
     behavior: "smooth",
     left: 0,
-    top: elem.getBoundingClientRect().top + global.window.pageYOffset,
+    top: elem.getBoundingClientRect().top + window.pageYOffset,
   });
 };
 
 export const smoothScrollAsync = (to: number, duration: number) => {
   const element =
-      global.window.document.scrollingElement ||
-      global.window.document.documentElement,
+      window.document.scrollingElement || window.document.documentElement,
     start = element.scrollTop,
     change = to - start,
     startDate = +new Date();
@@ -44,7 +43,7 @@ export const scrollToElemAsync = async (
   elemSelector: string
 ): Promise<void> => {
   if (elemSelector) {
-    const elem = global.window.document.querySelector(elemSelector);
+    const elem = window.document.querySelector(elemSelector);
 
     await smoothScrollAsync((elem as HTMLElement).offsetTop, 1000);
   }
@@ -52,25 +51,34 @@ export const scrollToElemAsync = async (
 
 export const smoothScrollTo = (to: number, duration: number) => {
   const element =
-      global.window.document.scrollingElement ||
-      global.window.document.documentElement,
-    start = element.scrollTop,
-    change = to - start,
-    startDate = +new Date();
+    window.document.scrollingElement || window.document.documentElement;
 
-  const easeInOutQuad = (t: any, b: any, c: any, d: any) => {
-    t /= d / 2;
-    if (t < 1) return (c / 2) * t * t + b;
-    t--;
-    return (-c / 2) * (t * (t - 2) - 1) + b;
+  const start = element.scrollTop;
+
+  const change = to - start;
+
+  const startDate = +new Date();
+
+  const easeInOutQuad = (
+    currentTime: number,
+    startValue: number,
+    changeInValue: number,
+    duration: number
+  ) => {
+    currentTime /= duration / 2;
+    if (currentTime < 1)
+      return (changeInValue / 2) * currentTime * currentTime + startValue;
+    currentTime--;
+    return (
+      (-changeInValue / 2) * (currentTime * (currentTime - 2) - 1) + startValue
+    );
   };
 
   const animateScroll = () => {
     const currentDate = +new Date();
     const currentTime = currentDate - startDate;
-    element.scrollTop = parseInt(
-      easeInOutQuad(currentTime, start, change, duration)
-    );
+    element.scrollTop = easeInOutQuad(currentTime, start, change, duration);
+
     if (currentTime < duration) {
       requestAnimationFrame(animateScroll);
     } else {
@@ -83,14 +91,14 @@ export const smoothScrollTo = (to: number, duration: number) => {
 const supportsNativeSmoothScroll =
   typeof window === "undefined"
     ? null
-    : "scrollBehavior" in global.window.document.documentElement.style;
+    : "scrollBehavior" in window.document.documentElement.style;
 
 export const scrollToElem = (elemSelector: string): void => {
   if (!elemSelector) {
     return;
   }
 
-  const elem = global.window.document.querySelector(elemSelector);
+  const elem = window.document.querySelector(elemSelector);
   if (elem) {
     if (supportsNativeSmoothScroll as any) {
       nativeSmoothScrollTo(elem);
